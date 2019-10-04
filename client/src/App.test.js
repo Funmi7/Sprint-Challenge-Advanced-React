30f7df88782f492498155a72713ed0c54e9414a8
Funmi7 <talabifunmilayo@gmail.com>
2019-10-04 10:41:25 +0100

    Wrote some integration tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import DarkModeSetter from './components/DarkModeSetter';
+import PlayersList from './components/PlayersList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // ReactDOM.unmountComponentAtNode(div);
 });
+
+describe("DarkModeSetter", () => {
+  it("DarkModeSetter renders without crashing", () => {
+    const div=document.createElement("div");
+    ReactDOM.render(<DarkModeSetter />, div);
+  });
+});
+
+// describe("playersList", () => {
+//   it('PlayersList renders without crashing', () => {
+//     const div=document.createElement("div");
+//     ReactDOM.render(<PlayersList />, div);
+//   });
+// });
+
+
+
+
+
+
